235f1936f42875369f32f1b8a0ce99e1643e3296
Ariesrys <ariestyaeka27@gmail.com>
2023-11-15 14:57:59 +0800

    membuat Register Controller

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class UserController extends BaseController
 {
@@ -23,5 +24,40 @@ class UserController extends BaseController
         ];
 
         return view('adminPage/add_user_form', $page);
+    }
+    public function register()
+    {
+        $data = [];
+
+        if ($this->request->getPost() === 'post') {
+            $rules = [
+                'username' => 'required|min_length[3]|max_length[100]',
+                'email' => 'required|valid_email|is_unique[users.email]',
+                'nomor_telpon' => 'required|min_length[10]|max_length[20]',
+                'password' => 'required|min_length[6]|max_length[255]',
+            ];
+
+            if ($this->validate($rules)) {
+                $model = new UserModel();
+
+                $newData = [
+                    'username' => $this->request->getPost('username'),
+                    'email' => $this->request->getPost('email'),
+                    'nomor_telepon' => $this->request->getPost('nomor_telpon'),
+                    'password' => $this->request->getPost('password'),
+                    'role' => 'admin-desa',
+                    'id-desa' => 1
+                ];
+
+                $model->save($newData);
+
+                // Sesuaikan alur navigasi sesuai kebutuhan aplikasi Anda.
+                return redirect()->to('/')->with('success', 'User registered successfully');
+            } else {
+                $data['validation'] = $this->validator;
+            }
+        }
+
+        return view('authPage/auth-register', $data);
     }
 }
--- app/Views/authPage/auth-register.php
+++ app/Views/authPage/auth-register.php
@@ -6,98 +6,99 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign up</title>
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.css') ?>">
-    
+
     <!-- <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon"> -->
     <link rel="stylesheet" href="<?= base_url('assets/css/app.css') ?>">
 </head>
 <style>
-    .garamond{
+    .garamond {
         font-family: 'Garamond', serif;
     }
 </style>
 
 <body>
     <div id="auth">
-        
-<div class="container">
-    <div class="row">
-        <div class="col-md-7 col-sm-12 mx-auto">
-            <div class="card pt-4">
-                <div class="card-body">
-                    <div class="text-center mb-5">
-                        <!-- <img src="assets/images/favicon.svg" height="48" class='mb-4'> -->
-                        <h3>Registrasi</h3>
-                    </div>
-                    <form class="form form-horizontal">
-                        <div class="form-body"> 
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <label>Nama Pengguna</label>
-                                </div>
-                                <div class="col-md-8 form-group">
-                                    <input type="text" id="username" class="form-control" name="username">
-                                </div>
-                                <div class="col-md-4">
-                                    <label>Email</label>
-                                </div>
-                                <div class="col-md-8 form-group">
-                                    <input type="email" id="email-id" class="form-control" name="email-id">
-                                </div>
-                                <div class="col-md-4">
-                                    <label>Nomer Telepon</label>
-                                </div>
-                                <div class="col-md-8 form-group">
-                                    <input type="text" id="nomer-hp" class="garamond form-control" name="contact">
-                                </div>
-                                <div class="col-md-4">
-                                    <label>Password</label>
-                                </div>
-                                <div class="col-md-8 form-group">
-                                    <input type="password" id="password" class="form-control" name="password" placeholder="Password">
-                                </div>
-                                <div class="col-md-4">
-                                    <label>Kecamatan</label>
-                                </div>
-                                <div class="col-md-8 form-group">
-                                    <select id="Kecamatan" name="Kecamatan" class="form-select">
-                                        <option value="" selected disabled>Pilih Kecamatan</option>
-                                        <option class="dropdown-item" value="kecamatan1">Kecamatan 1</option>
-                                        <option class="dropdown-item" value="kecamatan2">Kecamatan 2</option>
-                                        <!-- Tambahkan opsi-opsi lainnya sesuai kebutuhan -->
-                                    </select>
-                                </div>
-                                <div class="col-md-4">
-                                    <label>Desa</label>
-                                </div>
-                                <div class="col-md-8 form-group">
-                                    <select id="Desa" name="Desa" class="form-select">
-                                        <option value="" selected disabled>Pilih Desa</option>
-                                        <option class="dropdown-item" value="kecamatan1">Desa 1</option>
-                                        <option class="dropdown-item" value="kecamatan2">Desa 2</option>
-                                        <!-- Tambahkan opsi-opsi lainnya sesuai kebutuhan -->
-                                    </select>
-                                    <!-- <button type="reset" class="btn btn-light-secondary mr-1 mb-1">Reset</button> -->
-                                </div>
-                                <a href="<?= base_url('/'); ?>">Have an account? Login</a>
-                                <div class="col-sm-12 d-flex justify-content-end">
-                                    <button type="submit" class="btn btn-primary mr-1 mb-1">Daftar</button>
-                                </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7 col-sm-12 mx-auto">
+                    <div class="card pt-4">
+                        <div class="card-body">
+                            <div class="text-center mb-5">
+                                <!-- <img src="assets/images/favicon.svg" height="48" class='mb-4'> -->
+                                <h3>Registrasi</h3>
                             </div>
+                            <form class="form form-horizontal" action="/auth/register" method="post">
+                                <?= csrf_field() ?>
+                                <div class="form-body">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <label>Nama Pengguna</label>
+                                        </div>
+                                        <div class="col-md-8 form-group">
+                                            <input type="text" id="username" class="form-control" name="username">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Email</label>
+                                        </div>
+                                        <div class="col-md-8 form-group">
+                                            <input type="email" id="email-id" class="form-control" name="email">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Nomer Telepon</label>
+                                        </div>
+                                        <div class="col-md-8 form-group">
+                                            <input type="text" id="nomer-hp" class="garamond form-control" name="nomor_telpon">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Password</label>
+                                        </div>
+                                        <div class="col-md-8 form-group">
+                                            <input type="password" id="password" class="form-control" name="password" placeholder="password">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Kecamatan</label>
+                                        </div>
+                                        <div class="col-md-8 form-group">
+                                            <select id="Kecamatan" name="Kecamatan" class="form-select">
+                                                <option value="" selected disabled>Pilih Kecamatan</option>
+                                                <option class="dropdown-item" value="kecamatan1">Kecamatan 1</option>
+                                                <option class="dropdown-item" value="kecamatan2">Kecamatan 2</option>
+                                                <!-- Tambahkan opsi-opsi lainnya sesuai kebutuhan -->
+                                            </select>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Desa</label>
+                                        </div>
+                                        <div class="col-md-8 form-group">
+                                            <select id="Desa" name="Desa" class="form-select">
+                                                <option value="" selected disabled>Pilih Desa</option>
+                                                <option class="dropdown-item" value="kecamatan1">Desa 1</option>
+                                                <option class="dropdown-item" value="kecamatan2">Desa 2</option>
+                                                <!-- Tambahkan opsi-opsi lainnya sesuai kebutuhan -->
+                                            </select>
+                                            <!-- <button type="reset" class="btn btn-light-secondary mr-1 mb-1">Reset</button> -->
+                                        </div>
+                                        <a href="<?= base_url('/'); ?>">Have an account? Login Here</a>
+                                        <div class="col-sm-12 d-flex justify-content-end">
+                                            <input type="submit" class="btn btn-primary mr-1 mb-1" value="Register">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
     </div>
     <script src="assets/js/feather-icons/feather.min.js"></script>
     <script src="assets/js/app.js"></script>
-    
+
     <script src="assets/js/main.js"></script>
     <script>
-        document.getElementById('nomer-hp').addEventListener('input', function (event) {
+        document.getElementById('nomer-hp').addEventListener('input', function(event) {
             let inputValue = event.target.value.replace(/\D/g, ''); // Remove non-numeric characters
             const maxLength = 13;
 
